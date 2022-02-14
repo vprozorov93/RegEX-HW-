41f4e2c9ca8703c425e2e31924aa19575613988a
Vladislav Prozorov <prozorov93@gmail.com>
2022-02-14 15:57:33 +0300

    Update process_doubles method

--- EditPhoneBook.py
+++ EditPhoneBook.py
@@ -1,6 +1,6 @@
 import csv
-from prettytable import PrettyTable
 import re
+from prettytable import PrettyTable
 
 
 class EditPhoneBook:
@@ -30,32 +30,39 @@ class EditPhoneBook:
                         count_index += 1
 
     def process_doubles(self):
-        cache_list = [row[:2] for row in self.contacts_list[1:]]
-        double_list = [cache_list.pop(cache_list.index(el)) for el in cache_list if cache_list.count(el) > 1]
-        double_list = [row for index, row in enumerate(self.contacts_list) if row[:2] in double_list]
+        # short execution time
+        fixed_info_list = []
+        for i in range(len(self.contacts_list)):
+            for j in range(len(self.contacts_list)):
+                if self.contacts_list[i][:2] == self.contacts_list[j][:2]:
+                    self.contacts_list[i] = [x or y for x, y in zip(self.contacts_list[i], self.contacts_list[j])]
+            if self.contacts_list[i] not in fixed_info_list:
+                fixed_info_list.append(self.contacts_list[i])
+        fixed_info_list.sort()
+        self.contacts_list = fixed_info_list
 
-        for row in double_list:
-            self.contacts_list.remove(row)
-
+    def process_doubles_v2(self):
+        # short code
         double_name_dict = {}
-        for row in double_list:
+        for row in self.contacts_list:
             if not double_name_dict.get(row[0] + row[1]):
                 double_name_dict[row[0] + row[1]] = [row]
             else:
                 double_name_dict[row[0] + row[1]].append(row)
 
+        self.contacts_list.clear()
+        result = self.contacts_list
         count_dict = 0
-        result = []
+
         for key, values in double_name_dict.items():
             result.append(values.pop(0))
-
-            for row in values:
-                for index, column in enumerate(row):
-                    if not result[count_dict][index]:
-                        result[count_dict][index] = column
+            if len(values) > 0:
+                for row in values:
+                    for index, column in enumerate(row):
+                        if not result[count_dict][index]:
+                            result[count_dict][index] = column
             count_dict += 1
-
-        self.contacts_list.extend(result)
+        result.sort()
 
     def reformat_phone_numbers(self):
         regex_pattern = r"(\+7|8)\s?\(?(\d{3})\)?\s?\-?(\d+)s?\-?(\d+)s?\-?(\d+)\s?\(?(доб.)?\s?(\d+)?\)?"
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from EditPhoneBook import EditPhoneBook as EPB
 
 if __name__ == "__main__":
     phone_book = EPB("phonebook_raw.csv")
-    phone_book.process_and_save_phone_book(True)
+    phone_book.process_and_save_phone_book(False)
